22f5015c5e0f1df5944564f8f683eeeda6d0d543
Rob Peck <rob@robpeck.com>
2018-12-20 09:22:58 -0600

    Initial Commit

---
+++ PlayerControls/PlayerControls.h
@@ -0,0 +1,19 @@
+//
+//  PlayerControls.h
+//  PlayerControls
+//
+//  Created by Rob Peck on 12/20/18.
+//  Copyright © 2018 Rob Peck. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PlayerControls.
+FOUNDATION_EXPORT double PlayerControlsVersionNumber;
+
+//! Project version string for PlayerControls.
+FOUNDATION_EXPORT const unsigned char PlayerControlsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayerControls/PublicHeader.h>
+
+
