0fa5ff84ebd43b1f44f25750b0e67f2bf2b9b2cf
Rob Peck <rob@robpeck.com>
2018-12-22 09:58:36 -0500

    Total time and current time.

--- PlayerControlsDemo/ViewController.swift
+++ PlayerControlsDemo/ViewController.swift
@@ -22,14 +22,28 @@ class ViewController: NSViewController {
             self.playerControl.transferred = self.tranferred / 100
         }
     }
+    
+    @objc var totalTime: NSNumber = 0 {
+        didSet {
+            self.playerControl.totalTime = totalTime.doubleValue
+        }
+    }
+    
+    @objc var currentTime: NSNumber = 0 {
+        didSet {
+            self.playerControl.currentTime = currentTime.doubleValue
+        }
+    }
 
     @IBOutlet weak var containerLayer: NSView!
     @IBOutlet weak var playerControl: PlayerControl!
+    @IBOutlet weak var actionLabel: NSTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.containerLayer.wantsLayer = true
         self.containerLayer.layer?.backgroundColor = self.viewBackgroundColor.cgColor
+        self.playerControl.delegate = self
     }
     
     @IBAction func themeChanged(_ sender: Any?) {
@@ -43,3 +57,22 @@ class ViewController: NSViewController {
     }
 }
 
+extension ViewController: PlayerControlDelegate {
+    func statusChanged(_ playerControl: PlayerControl, status: PlayerControl.Status) {
+        var playerStatusString = ""
+        switch status {
+        case .playing:
+            playerStatusString = "playing"
+        case .paused:
+            playerStatusString = "paused"
+        }
+        
+        self.actionLabel.stringValue = "statusChanged " + playerStatusString
+    }
+    
+    func timeChanged(_ playerControl: PlayerControl, time: TimeInterval) {
+        self.actionLabel.stringValue = "timeChanged " + String(time)
+    }
+    
+    
+}
