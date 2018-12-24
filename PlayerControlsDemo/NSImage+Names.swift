c2b0ff6057a619b0eb00f971e34d19bf6b597036
Rebecca Peck <me@rebeccapeck.org>
2018-12-23 21:25:38 -0600

    General code cleanup and demo app wor.

---
+++ PlayerControlsDemo/NSImage+Names.swift
@@ -0,0 +1,19 @@
+//
+//  NSImage+Names.swift
+//  PlayerControlsDemo
+//
+//  Created by Rob Peck on 12/23/18.
+//  Copyright © 2018 Rob Peck. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+extension NSImage.Name {
+    static let play = NSImage.Name("play")
+    static let pause = NSImage.Name("pause")
+    static let rewind = NSImage.Name("rewind")
+    static let ff = NSImage.Name("ff")
+    static let beginning = NSImage.Name("beginning")
+    static let end = NSImage.Name("end")
+}
