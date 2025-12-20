b40d4294ee5f8657871da56f00c20243001684f0
Rebecca Peck <me@rebeccapeck.org>
2025-12-20 00:12:55 -0700

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PlayerControls",
+    platforms: [
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "PlayerControls",
+            targets: ["PlayerControls"]),
+    ],
+    targets: [
+        .target(
+            name: "PlayerControls",
+            dependencies: [],
+            path: "PlayerControls",
+            exclude: []
+        ),
+        .testTarget(
+            name: "PlayerControlsTests",
+            dependencies: ["PlayerControls"],
+            path: "PlayerControlsTests"
+        ),
+    ]
+)
